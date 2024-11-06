ba56e2985a34ce1051145a1a769527503c17d32e
AngelSaul123 <angelsaulhernandezrobles@gmail.com>
2024-11-05 21:55:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function downloadApp() {
+    window.location.href = 'link-de-descarga-de-la-app'; // Coloca aquí el enlace de descarga de la app
+}
+
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+function contact() {
+    alert("Contáctanos al número: 2381238229");
+}
